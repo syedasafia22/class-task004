ed05a0026f121b877507f5902a7b17a364521489
syedasafia22 <syedasafia2003@gmail.com>
2024-09-29 16:44:55 +0500

    datainput

---
+++ age.js
@@ -0,0 +1,7 @@
+function ageCalculate(){
+    let today= new Date();
+    let inputDate = new Date(document.getElementById ("date.input").value);
+    let birthMonth, birthDate, birthYear;
+    let birthDetails();
+    
+}
