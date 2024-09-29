bf6a436afacb3a469bc964fbed269fc6203aef9b
syedasafia22 <syedasafia2003@gmail.com>
2024-09-29 16:53:19 +0500

    js; current command year

--- age.js
+++ age.js
@@ -2,6 +2,20 @@ function ageCalculate(){
     let today= new Date();
     let inputDate = new Date(document.getElementById ("date.input").value);
     let birthMonth, birthDate, birthYear;
-    let birthDetails();
-    
+    let birthDetails={
+        date:inputDate.getDate(),
+        month:inputDate.getMonth()+1,
+        year:inputDate.getFullYear()
+    }
+    let currentYear=
+    today.getFullYear();
+    let currentMonth=
+    today.getMonth()+1;
+    leapChecker(currentYear);
+    if(
+        birthDates.year>currentYear||
+        (birt)
+    )
+
+
 }
